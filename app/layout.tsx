fbfb6db4867b410bbbc2c15be49570913db25090
AlexLeiba <Alexleiba13@gmail.com>
2025-08-12 18:44:42 +0300

    burger button fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,41 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import QueryProvider from '@/components/providers/QueryProvider';
-import { Toaster } from 'sonner';
-import { Navbar } from '@/components/Navbar';
-import { NavbarLanding } from '@/components/NavbarLanding';
+import QueryProvider from "@/components/providers/QueryProvider";
+import { Toaster } from "sonner";
+import { Navbar } from "@/components/Navbar";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Spend||Keep',
+  title: "Spend||Keep",
   description:
-    'Spend or keep is a budget tracker app that helps you track your expenses and stay on top of your budget.',
+    "Spend or keep is a budget tracker app that helps you track your expenses and stay on top of your budget.",
 
   // Open Graph
   openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://spend-or-keep.vercel.app/',
-    title: 'Spend||Keep',
+    type: "website",
+    locale: "en_US",
+    url: "https://spend-or-keep.vercel.app/",
+    title: "Spend||Keep",
     description:
-      'Spend or keep is a budget tracker web app that helps you track your expenses and stay on top of your budget.',
+      "Spend or keep is a budget tracker web app that helps you track your expenses and stay on top of your budget.",
     images: [
       {
-        url: 'https://res.cloudinary.com/deixj28ym/image/upload/v1754381004/travel-plan/m8w8ykg507evpnasawte.webp',
+        url: "https://res.cloudinary.com/deixj28ym/image/upload/v1754381004/travel-plan/m8w8ykg507evpnasawte.webp",
         width: 1200,
         height: 630,
-        alt: 'Spend||Keep',
+        alt: "Spend||Keep",
       },
     ],
   },
@@ -49,18 +48,18 @@ export default function RootLayout({
 }>) {
   return (
     <QueryProvider>
-      <ClerkProvider afterSignOutUrl={'/sign-in'}>
-        <html lang='en' className=''>
+      <ClerkProvider afterSignOutUrl={"/sign-in"}>
+        <html lang="en" className="">
           <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-background dark:text-white`}
           >
-            <NavbarLanding />
+            <Navbar />
             {children}
           </body>
         </html>
       </ClerkProvider>
 
-      <Toaster position='bottom-right' />
+      <Toaster position="bottom-right" />
     </QueryProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,14 @@
-'use client';
-import React, { useEffect } from 'react';
-import { Logo } from './Logo';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
-import { ThemeToggle } from '@/lib/ThemeToggle';
-import { UserButton, useUser } from '@clerk/nextjs';
-import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
-import { Menu } from 'lucide-react';
-import { DialogTitle } from './ui/dialog';
+"use client";
+import React, { useEffect } from "react";
+import { Logo } from "./Logo";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { ThemeToggle } from "@/lib/ThemeToggle";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+import { DialogTitle } from "./ui/dialog";
 
 export function Navbar() {
   const [hasUser, setHasUser] = React.useState(false);
@@ -18,16 +18,16 @@ export function Navbar() {
 
   const linkItems = [
     {
-      label: 'Dashboard',
-      href: '/dashboard',
+      label: "Dashboard",
+      href: "/dashboard",
     },
     {
-      label: 'Transactions',
-      href: '/transactions',
+      label: "Transactions",
+      href: "/transactions",
     },
     {
-      label: 'Manage',
-      href: '/manage',
+      label: "Manage",
+      href: "/manage",
     },
   ];
 
@@ -37,15 +37,15 @@ export function Navbar() {
   return (
     <>
       {/* Desktop Navbar */}
-      <div className='hidden border-separate border-b md:block  right-0 left-0 z-50  dark:bg-gray-950 bg-gray-300 fixed top-0'>
-        <div className=' max-w-5xl mx-auto  '>
-          <nav className='glex items-center justify-between px-8'>
-            <div className='flex h-[65px] min-h-[60px] items-center gap-x-8'>
+      <div className="hidden border-separate border-b md:block  right-0 left-0 z-50  dark:bg-gray-950 bg-gray-300 fixed top-0">
+        <div className=" max-w-5xl mx-auto  ">
+          <nav className="glex items-center justify-between px-8">
+            <div className="flex h-[65px] min-h-[60px] items-center gap-x-8">
               <Logo />
 
               {/* links on dashboard */}
-              {pathname !== '/wizard' ? (
-                <div className='flex w-full gap-4 items-center'>
+              {pathname !== "/wizard" ? (
+                <div className="flex w-full gap-4 items-center">
                   {hasUser &&
                     linkItems.map((data, index) => {
                       const isActive = pathname === data.href.toLowerCase();
@@ -55,16 +55,16 @@ export function Navbar() {
                           href={data.href}
                           className={cn(
                             isActive
-                              ? 'font-bold dark:text-white text-black '
-                              : 'font-medium',
-                            'relative hover:bg-gray-200  transition-all p-2 rounded-md  text-gray-700 dark:hover:text-black dark:text-gray-100 '
+                              ? "font-bold dark:text-white text-black "
+                              : "font-medium",
+                            "relative hover:bg-gray-200  transition-all p-2 rounded-md  text-gray-700 dark:hover:text-black dark:text-gray-100 "
                           )}
                         >
                           <p className={cn()}>{data.label}</p>
                           <div
                             className={cn(
                               isActive &&
-                                'border-b-1 dark:border-white border-black absolute top-[52px] left-1/2 -translate-1/2 bottom-0  w-[120%] z-10'
+                                "border-b-1 dark:border-white border-black absolute top-[52px] left-1/2 -translate-1/2 bottom-0  w-[120%] z-10"
                             )}
                           ></div>
                         </Link>
@@ -72,13 +72,13 @@ export function Navbar() {
                     })}
                 </div>
               ) : (
-                <div className='w-full'></div>
+                <div className="w-full"></div>
               )}
 
-              <div className='flex items-center gap-4'>
+              <div className="flex items-center gap-4">
                 <ThemeToggle />
                 {hasUser && (
-                  <div className='flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full'>
+                  <div className="flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full">
                     <UserButton />
                   </div>
                 )}
@@ -89,31 +89,31 @@ export function Navbar() {
       </div>
 
       {/* Mobile Navbar */}
-      <div className='block    md:hidden  fixed top-0 left-0 right-0  dark:bg-gray-950 bg-gray-300'>
-        <nav className='flex  px-6 h-[60px] min-h-[60px] items-center justify-between w-full'>
-          <div className='flex justify-between items-center w-full'>
+      <div className="block    md:hidden  fixed top-0 left-0 right-0  dark:bg-gray-950 bg-gray-300">
+        <nav className="flex  px-6 h-[60px] min-h-[60px] items-center justify-between w-full">
+          <div className="flex justify-between items-center w-full">
             {/* links */}
-            <div className=''>
+            <div className="">
               <Sheet
                 open={isOpenedMenu}
                 onOpenChange={() => setIsOpenedMenu(!isOpenedMenu)}
               >
                 <SheetTrigger
                   asChild
-                  className='cursor-pointer w-5 h-5 flex justify-end items-center '
+                  className="cursor-pointer w-5 h-5 flex justify-end items-center "
                 >
                   {hasUser && (
                     <Menu
                       onClick={() => setIsOpenedMenu(!isOpenedMenu)}
-                      className='w-5 h-5 '
+                      className="w-5 h-5 "
                     />
                   )}
                 </SheetTrigger>
-                <SheetContent className='py-6 px-4' side='left'>
+                <SheetContent className="py-6 px-4" side="left">
                   <DialogTitle>Menu</DialogTitle>
-                  <div className='flex items-center gap-2'>
-                    <p className='text-xs'>Profile</p>
-                    <div className='flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full'>
+                  <div className="flex items-center gap-2">
+                    <p className="text-xs">Profile</p>
+                    <div className="flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full">
                       <UserButton />
                     </div>
                   </div>
@@ -123,15 +123,15 @@ export function Navbar() {
                       <Link
                         key={index}
                         href={data.href}
-                        className='relative'
+                        className="relative"
                         onClick={() => setIsOpenedMenu(false)}
                       >
                         <p
                           className={cn(
                             isActive
-                              ? 'font-bold dark:text-white'
-                              : 'font-light text-gray-100',
-                            '  text-gray-700 dark:text-gray-200 hover:text-primary'
+                              ? "font-bold dark:text-white"
+                              : "font-light text-gray-100",
+                            "  text-gray-700 dark:text-gray-200 hover:text-primary"
                           )}
                         >
                           {data.label}
@@ -139,15 +139,16 @@ export function Navbar() {
                       </Link>
                     );
                   })}
-                  <div className='flex items-center gap-4'>
+                  <div className="flex items-center gap-4">
                     <ThemeToggle />
                   </div>
                 </SheetContent>
               </Sheet>
             </div>
             <Logo />
+            {!hasUser && <ThemeToggle />}
             {hasUser ? (
-              <div className='flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full'>
+              <div className="flex w-8 h-8 justify-center items-center dark:bg-gray-300 bg-gray-700 rounded-full">
                 <UserButton />
               </div>
             ) : (
